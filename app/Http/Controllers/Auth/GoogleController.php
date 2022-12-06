66e86c35a7a2a61341fcdf47af37c465b6896536
nan74k <nanta.pslove@gmail.com>
2022-12-07 01:10:53 +0700

    asd

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -14,7 +14,7 @@ class GoogleController extends Controller
 {
     public function login()
     {
-        return Socialite::driver('google')->redirect();
+        return Socialite::driver('google')->redirect()->route('home');
     }
 
     public function callback(){
